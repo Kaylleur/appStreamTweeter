0cab7fb37c79a7b5ccc9fd73c52ee963c36f3073
kaylleur <thomas85340@gmail.com>
2015-12-17 16:44:47 +0100

    sort by language

--- models/tweet.js
+++ models/tweet.js
@@ -2,14 +2,17 @@ var Twitter = require('twitter');
 var db = require('./elastic');
 var fs = require('fs');
 
-var client = new Twitter(JSON.parse(fs.readFileSync('/config.json', 'utf8')));
+var client = new Twitter(JSON.parse(fs.readFileSync('./config.json', 'utf8')));
 
 
 module.exports = {
     listen : function(hashtag){
-        var params = {track: hashtag};
+        var params = {track: hashtag,language:"en"};
         client.stream('statuses/filter', params, function(stream){
-            stream.on('data', function(tweet) {
+            stream.on('data', function(tweet,err) {
+                if(err){
+                    console.log(err);
+                }
                 db.create(params.track,tweet);
             });
         });
